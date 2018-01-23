abdc38d2b5566d1119d1a8409537e7fde9058fae
Praveen Kumar Verma <pkv121@gmail.com>
2018-01-24 00:19:29 +0530

    Basic structure

---
+++ backend/config/views.py
@@ -0,0 +1,12 @@
+from rest_framework import status, serializers, views
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+class MessageSerializer(serializers.Serializer):
+    message = serializers.CharField()
+
+class EchoView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = MessageSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
