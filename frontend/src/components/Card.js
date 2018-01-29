a713a5570f734360de99c14fd55a640f9bbf0a51
Praveen Kumar Verma <pkv121@gmail.com>
2018-01-30 00:35:17 +0530

    Add Featured section on home page

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,6 +4,7 @@ const Card = (props) => {
   return (
   	<div className="col-md-4 mb-4">
       <div className="card h-100">
+        <img className="card-img-top" src="..." alt="Card image cap"/>
         <div className="card-body">
           <h2 className="card-title">Card One</h2>
           <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
