708971e23e9ba7a0405730157e5397ee1cd85404
Praveen Kumar Verma <pkv121@gmail.com>
2018-01-30 23:23:43 +0530

    Add dummy images

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import createHistory from 'history/createBrowserHistory'
-import { ConnectedRouter } from 'react-router-redux'
-import { Provider } from 'react-redux'
-import {Route, Switch} from 'react-router'
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import {Route, Switch} from 'react-router';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 import App from './App';
 import About from './components/About';
-import PrivateRoute from './containers/PrivateRoute';
-import configureStore from './store'
+import configureStore from './store';
 
 import Login from './containers/Login';
 const history = createHistory()
@@ -23,7 +23,6 @@ ReactDOM.render((
         <Route exact path="/" component={App} />
         <Route exact path="/about" component={About} />
         <Route exact path="/login" component={Login} />
-        <PrivateRoute path="/" component={App}/>
       </Switch>
     </ConnectedRouter>
   </Provider>
