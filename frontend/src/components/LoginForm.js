9ad71ebf9233e9ddb27173d4d1e97782a7a3b48f
raj <raj123pant@gmail.com>
2018-01-31 00:11:45 +0530

    Fix login page.

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,47 +1,66 @@
-import React, {Component} from 'react'
-import { Alert, Button,  Form } from 'reactstrap';
+import React, { Component } from 'react';
+import { Alert, Button, Form } from 'reactstrap';
 
-import TextInput from './TextInput'
+import TextInput from './TextInput';
 
 export default class LoginForm extends Component {
   state = {
     username: '',
     password: ''
-  }
+  };
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     const target = event.target;
-    const value = target.type ===
-        'checkbox' ? target.checked : target.value;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
 
     this.setState({
       [name]: value
     });
-  }
+  };
 
   componentDidMount() {
     //this.primaryInput?this.primaryInput.focus():'';
   }
 
-  onSubmit = (event) => {
-    event.preventDefault()
-    this.props.onSubmit(this.state.username, this.state.password)
-  }
+  onSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.username, this.state.password);
+  };
 
   render() {
-    const errors = this.props.errors || {}
+    const errors = this.props.errors || {};
 
     return (
       <div className="container">
         <Form onSubmit={this.onSubmit}>
           <h2>Login Here</h2>
-          {errors.non_field_errors?<Alert color="danger">{errors.non_field_errors}</Alert>:""}
-          <TextInput name="username" label="Username" error={errors.username} onChange={this.handleInputChange}/>
-          <TextInput name="password" label="Password" error={errors.password} type="password" onChange={this.handleInputChange}/>
-          <Button type="submit" color="primary">Log In</Button>
+          {errors.non_field_errors ? (
+            <Alert color="danger">{errors.non_field_errors}</Alert>
+          ) : (
+            ''
+          )}
+          <TextInput
+            name="username"
+            label="Username"
+            error={errors.username}
+            onChange={this.handleInputChange}
+          />
+          <TextInput
+            name="password"
+            label="Password"
+            error={errors.password}
+            type="password"
+            onChange={this.handleInputChange}
+          />
+          <Button type="submit" color="primary">
+            Log In
+          </Button>
+          <a href="#" style={{ float: 'right', paddingTop: 8 }}>
+            Forgot password
+          </a>
         </Form>
       </div>
-    )
+    );
   }
 }
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,40 +1,49 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Redirect } from 'react-router'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 
-import LoginForm from '../components/LoginForm'
-import {login} from  '../actions/auth'
-import {authErrors, isAuthenticated} from '../reducers'
-import Navigation from '../components/Navigation'
-const Login = (props) => {
-  if(props.isAuthenticated) {
-     return  <Redirect to='/' />
+import LoginForm from '../components/LoginForm';
+import { login } from '../actions/auth';
+import { authErrors, isAuthenticated } from '../reducers';
+import Navigation from '../components/Navigation';
+
+const Login = props => {
+  if (props.isAuthenticated) {
+    return <Redirect to="/" />;
   }
 
   return (
     <div>
-      <Navigation/>
-      <div className="row">
-      <div className="col-md-3 col-md-offset-4">
-        <div className="login-page">
-
-         <LoginForm {...props}/>
+      <Navigation />
+      <section className="login-block" style={{ marginTop: 80 }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <LoginForm {...props} />
+            </div>
+            <div className="col-md-8 right-sec">
+              <img
+                className="img-fluid"
+                src="http://placehold.it/900x400"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   errors: authErrors(state),
   isAuthenticated: isAuthenticated(state)
-})
+});
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   onSubmit: (username, password) => {
-    dispatch(login(username, password))
+    dispatch(login(username, password));
   }
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
