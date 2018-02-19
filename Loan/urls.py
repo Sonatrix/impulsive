5a8954babd20ffd484815dd2372b2764c2b4740d
Praveen Kumar Verma <praveenkumarverma121@gmail.com>
2018-02-20 00:13:38 +0530

    Added over view menu links for header

--- Loan/urls.py
+++ Loan/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
 #login views
 urlpatterns += [
     path('login/', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
-    path('logout/', auth_views.logout, {'template_name': 'registration/logged_out.html'}, name='logout'),
+    path('logout/', auth_views.logout, {'template_name': 'index.html'}, name='logout'),
     path('signup/', SignUp.as_view(), name='signup')
 ]
