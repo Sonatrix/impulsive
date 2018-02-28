414c02b6a25b6979aaa9c4ec695880d309185ca7
Praveen Kumar Verma <praveenkumarverma121@gmail.com>
2018-03-01 01:04:10 +0530

    Added new admin section

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, userDetail
 
 app_name = 'Admin'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('detail', userDetail, name='user_detail'),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request) :
 	return render(request, 'home.html', {})
+
+def userDetail(request):
+	return render(request, 'user/user_details.html', {})
